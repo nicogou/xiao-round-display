9449311a387519e1595bcb1897e64e1af5090efd
Nicolas Goualard <nicolas.goualard@sfr.fr>
2024-04-03 19:38:02 +0200

    Removed rotation things on the chsc6x driver because it is not needed. Use invert-y/invert-x in lvgl_pointer instead when needed.

--- drivers/input/chsc6x/input_chsc6x.c
+++ drivers/input/chsc6x/input_chsc6x.c
@@ -6,10 +6,6 @@
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/logging/log.h>
 
-#define DISPLAY_WIDTH         DT_INST_PROP(0, width)
-#define DISPLAY_HEIGHT        DT_INST_PROP(0, height)
-#define DISPLAY_ROTATION      DT_INST_PROP(0, rotation)
-
 struct chsc6x_config {
 	struct i2c_dt_spec i2c;
 	const struct gpio_dt_spec int_gpio;
@@ -28,18 +24,6 @@ struct chsc6x_output {
 } __attribute__((packed));
 
 LOG_MODULE_REGISTER(chsc6x, CONFIG_INPUT_LOG_LEVEL);
-
-static void chsc6x_convert_xy(uint16_t *x, uint16_t *y)
-{
-    uint16_t x_tmp = *x, y_tmp = *y, _end = 0;
-    for(int i=1; i<=DISPLAY_ROTATION/90; i++){
-        x_tmp = *x;
-        y_tmp = *y;
-        _end = (i % 2) ? DISPLAY_WIDTH : DISPLAY_HEIGHT;
-        *x = y_tmp;
-        *y = _end - x_tmp;
-    }
-}
 
 static int chsc6x_process(const struct device *dev)
 {
@@ -61,9 +45,6 @@ static int chsc6x_process(const struct device *dev)
 	is_pressed = output.points & 0xff;
 	col = sys_be16_to_cpu(output.x) & 0x00ff;
 	row = sys_be16_to_cpu(output.y) & 0x00ff;
-	if (is_pressed) {
-		chsc6x_convert_xy(&col, &row);
-	}
 	
 	LOG_DBG("Touched : %u - Column : %u - Row : %u", is_pressed, col, row);
 
